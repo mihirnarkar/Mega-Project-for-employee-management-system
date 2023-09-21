36a018ddf82f7df70f096a0001c4fcbab7fefbe0
Mihir Narkar <Mihirnarkar19@gmail.com>
2023-09-21 17:35:39 +0530

    on

--- EmployeeManagementSystem-frontend/src/app/employee1/employee1.component.ts
+++ EmployeeManagementSystem-frontend/src/app/employee1/employee1.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class Employee1Component {
 
+  employees = [
+    {id: 1,firstname: 'Mihir',lastname: 'Narkar',contactno: '8373788371',email: 'Mihirnarkar19@gmail.com',address:'Cotton green'},
+    {id: 2,firstname: 'Srushti',lastname: 'Pagare',contactno: '9899797991',email: 'srushti@gmail.com',address:'Kandivali'}
+  ];
+
+  editIndex: number = -1;
+  currentlyEditedEmployee: any = null;
+
+  addEmployee() {
+    const newEmployee = { id: this.employees.length + 1, firstname: '', lastname: '', contactno: '', email: '', address: '' };
+    this.employees.push(newEmployee);
+    this.editEmployee(this.employees.length - 1);
+  }
+
+  editEmployee(index: number) {
+    this.editIndex = index;
+    this.currentlyEditedEmployee = { ...this.employees[index] }; // Create a copy of the employee being edited
+  }
+
+  saveEmployee(index: number) {
+    if (this.currentlyEditedEmployee) {
+      this.employees[index] = { ...this.currentlyEditedEmployee }; // Update the employee data
+    }
+    this.editIndex = -1;
+    this.currentlyEditedEmployee = null;
+  }
+
+  cancelEdit(index: number) {
+    // Revert changes
+    this.editIndex = -1;
+    this.currentlyEditedEmployee = null;
+  }
+
+  deleteEmployee(index: number) {
+    this.employees.splice(index, 1);
+    this.editIndex = -1;
+    this.currentlyEditedEmployee = null;
+  }
 }
